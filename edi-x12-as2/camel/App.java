3bc82d2c0ab28de6d18f510e235e946b416100af
Aurélien Pupier <apupier@redhat.com>
2025-04-03 15:27:40 +0200

    Upgrade smooks example to 4.11.0
    Requires to adapt to API breaking change in Camel 4.11 related to the
    AS2
    content
    type
    
    see https://github.com/apache/camel/commit/5d964ff96680d58985250701352de1a384ab6806
    
    Signed-off-by: Aurélien Pupier <apupier@redhat.com>

--- edi-x12-as2/camel/App.java
+++ edi-x12-as2/camel/App.java
@@ -1,4 +1,4 @@
 //DEPS org.apache.activemq:artemis-jakarta-server:2.40.0
-//DEPS org.apache.camel:camel-http:4.10.0
+//DEPS org.apache.camel:camel-http:4.11.0
 //DEPS org.smooks.cartridges.edi:smooks-edi-cartridge:2.1.0
 //DEPS org.smooks.cartridges:smooks-templating-cartridge:2.1.2
