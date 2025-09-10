3f7b81e28a9dd74004a47095f6061348a487803b
Christoph Deppisch <cdeppisch@redhat.com>
2025-09-10 16:51:40 +0200

    Update to Camel 4.14.0 and Citrus 4.8.0 (#37)

--- edi-x12-as2/camel/App.java
+++ edi-x12-as2/camel/App.java
@@ -1,4 +1,4 @@
-//DEPS org.apache.activemq:artemis-jakarta-server:2.40.0
-//DEPS org.apache.camel:camel-http:4.11.0
+//DEPS org.apache.activemq:artemis-jakarta-server:2.42.0
+//DEPS org.apache.camel:camel-http:4.14.0
 //DEPS org.smooks.cartridges.edi:smooks-edi-cartridge:2.1.0
 //DEPS org.smooks.cartridges:smooks-templating-cartridge:2.1.2
